c8511ac6d5d128f6f1281df8f9e8552cc695f3fe
bryn <4146799+brynhayder@users.noreply.github.com>
2019-09-22 20:23:00 +0100

    updating

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bicep",
+    version="0.0.1",
+    author="brynhayder",
+    description="Tools for training PyTorch models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brynhayder/bicep",
+    packages=setuptools.find_packages(),
+    # The following may be untrue and needs to be checked!
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
